be3beb00c6b08915bf9d39f73e3479814f434274
JeanEric <ericjohn415@gmail.com>
2021-04-06 01:13:29 +0200

    used localStorage

--- counter.js
+++ counter.js
@@ -1,17 +1,20 @@
+if (!localStorage.getItem('counter')){
 
-let counter = 0;
+    localStorage.setItem('counter', 0);
+
+}
+
 function count()
 {
+    let counter = localStorage.getItem('counter');
     counter++;
     document.querySelector('h1').innerHTML = counter;
-    if (counter % 10 === 0){
-        alert(`count is now ${counter}`);
+    localStorage.setItem('counter', counter);
 
-
-    }
 }
 
 document.addEventListener('DOMContentLoaded', function(){
+    document.querySelector('h1').innerHTML = localStorage.getItem('counter');
 
     document.querySelector('button').onclick = count;
 });
